66d796d35a4d1afc4142058a0207e06136515033
Sohaib-Ahmed869 <i211114@nu.edu.pk>
2023-12-23 10:52:33 +0500

    Final Updates

--- src/components/Team.js
+++ src/components/Team.js
@@ -14,14 +14,16 @@ import elipse from '../assets/images/home/Ellipse.png'
 const Team = () => {
   return (
     <React.Fragment>
-      <div style={{textAlign: 'left', padding: '20px' }}>
-        <div id='team' style={{ backgroundImage: `url(${frame})`, height: '60vh', backgroundRepeat: 'no-repeat', backgroundSize: 'cover', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
-          <img src={elipse} alt="" className="img-fluid" style={{ marginLeft: '10px' }} />
-          <h2>Ready to begin your journey to unprecedented success?<br></br>
-            Start with our form below.</h2>
-          <button className="btn btn-primary" style={{ width: '200px', height: '50px', borderRadius: '10px', fontSize: '20px', fontWeight: 'bold', marginTop: '20px' }}>Go to Form</button>
+      <Container>
+      <div className="text-left" style={{textAlign: 'left',marginTop:'70px' }}>
+        <div id='team' style={{ backgroundImage: `url(${frame})`, height: '60vh', backgroundRepeat: 'no-repeat', backgroundSize: 'cover', display: 'flex', flexDirection: 'column',  justifyContent: 'center'}}>
+          <img src={elipse} alt="" className="img-fluid" style={{ marginLeft: '40px' , width: '60px', height: '30px'}} />
+          <h3>Ready to begin your journey to unprecedented success?<br></br>
+            Start with our form below.</h3>
+          <button className="btn btn-primary" style={{width:'200px',borderRadius: '10px', fontSize: '14px', marginTop: '20px' }}>Go to Form</button>
         </div>
       </div>
+      </Container>
 
     </React.Fragment>
   )
